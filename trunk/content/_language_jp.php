84e37fb7bdabdacbd56da047a6c6af56f07b4d4a
a2h <a2h@live.com.au>
2009-05-06 16:54:22 +1000

    Added Japanese language file as a test for the language system

---
+++ trunk/content/_language_jp.php
@@ -0,0 +1,72 @@
+﻿<?php
+/*
+	Ze Very Flat Pancaek CMS test version
+	Copyright 2009 a2h - http://a2h.uni.cc/
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+	
+	In case you're a lazy idiot who can't even at least get a basic
+	understanding of the license:
+	"You must retain, in the Source form of any Derivative Works that You
+	distribute, all copyright, patent, trademark, and attribution notices
+	from the Source form of the Work, excluding those notices that do not
+	pertain to any part of the Derivative Works"
+	
+	http://zvfpcms.sourceforge.net/
+*/
+
+// translated by a2h with google translate to test the language support
+// I DO NOT UNDERSTAND JAPANESE except for some basic things like yes/no etc
+// if you want to fix some strings please, do so.
+
+$txt['text_warning']              = '警告';
+$txt['text_success']              = '成功!';
+$txt['text_failure']              = '故障!';
+$txt['text_yes']                  = 'はい';
+$txt['text_no']                   = 'いいえ';
+$txt['text_notimplemented']       = '実装されていません！';
+
+$txt['zvfpcms_a2h']               = 'another creation from the strange mind of a2h';
+$txt['zvfpcms_powered']           = 'powered by zvfpcms';
+$txt['zvfpcms_generated']         = 'page generated in [t] seconds';
+
+$txt['admin_panel_title']         = '管理者パネル';
+$txt['admin_panel_noperms']       = 'このページにアクセスするために適切な権限がありません。';
+$txt['admin_panel_welcome']       = '管理者パネルへようこそ。';
+$txt['admin_panel_noedittitle']   = 'て、任意の既存のページのタイトルを変更しないでください。あなたはそれを手動で変更を取得する必要があります。';
+$txt['admin_panel_changepage']    = 'は、別のページに移動、変更を表示するには。';
+$txt['admin_panel_addpage']       = 'ページを追加';
+$txt['admin_panel_addpage_prog']  = 'ページを追加...';
+$txt['admin_panel_addpage_savpr'] = '保存ページ...';
+$txt['admin_panel_addpage_desc']  = 'このページでは、メニューの最後に追加されます。 セクションには、 "管理ページ"を使用して移動します';
+$txt['admin_panel_addpage_blnk']  = '（注：お使いのページこの情報が正しいことができない空白のコンテンツが）';
+$txt['admin_panel_manpages']      = '管理ページ';
+$txt['admin_panel_manpages_list'] = 'ページ';
+$txt['admin_panel_manpages_desc'] = 'このページの一覧です。';
+$txt['admin_panel_manpages_ordr'] = '使用[^]/[v]の順にメニューを移動します。';
+$txt['admin_panel_manpages_edit'] = 'をクリックして[e]は、ページを編集してください。';
+$txt['admin_panel_manpages_delt'] = 'をクリックして[d]のページを削除してください。';
+$txt['admin_panel_manbugs']       = 'バグ管理';
+$txt['admin_panel_confirm']       = 'あなたがこれを実行してもよろしいですか？';
+$txt['admin_panel_modmenu_prog']  = 'メニューの変更...';
+$txt['admin_panel_deleting']      = 'ファイルを削除する...';
+$txt['admin_panel_actn_noexist']  = 'その操作が存在しないか、実装されていません。';
+$txt['admin_panel_what']          = '何？';
+$txt['admin_panel_edt_src']       = '場合は、ソースをクリックして" HTML "のボタンを編集したい。';
+$txt['admin_panel_edt_srtnm']     = '短い名前';
+$txt['admin_panel_edt_srtnm_dsc'] = 'ただし、空白、首都。例えば \\\'testpg1\\\' を使用する代わりに \\\'test page 1\\\' 。英語の手紙/数字だけ。';
+$txt['admin_panel_edt_fllnm']     = '完全な名前';
+$txt['admin_panel_edt_fllnm_dsc'] = '完全名の両方のタイトルやメニュー名として表示される';
+$txt['admin_panel_edt_child']     = '子ページの';
+$txt['admin_panel_edt_child_dsc'] = 'あなたの子供のように、このページを設定したいページの短いタイトルを入力します。場合は、ページの子として、 -1 、このままこのセットを希望していません。';
+?>
