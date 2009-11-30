ee6354bab98a9a2ccecb91f301bf8ff03ca612fd
a2h <a2h@live.com.au>
2009-11-30 17:53:42 +1100

    Relicensed under the GNU General Public License v3

--- trunk/spongecms/admin/page_man_del.php
+++ trunk/spongecms/admin/page_man_del.php
@@ -1,22 +1,27 @@
 <?php
 /*
-	Sponge CMS
-	Copyright 2009 a2h - http://a2h.uni.cc/
+ * Sponge Content Management System
+ * Copyright (c) 2009 a2h - http://a2h.uni.cc/
+ * http://zvfpcms.sourceforge.net/
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Under section 7b of the GNU General Public License you are
+ * required to preserve this notice.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
-
-		http://www.apache.org/licenses/LICENSE-2.0
-
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
-	
-	http://a2h.github.com/Sponge-CMS/
-*/
 if ($zvfpcms)
 {
 	if (!isset($_GET['go']))
