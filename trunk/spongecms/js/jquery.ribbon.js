5ef6d194f870dc7b226cd3459e7ff5587fc28082
a2h <a2h@live.com.au>
2009-12-05 21:37:59 +1100

    Added indication that this version of the ribbon script is not official

--- trunk/spongecms/js/jquery.ribbon.js
+++ trunk/spongecms/js/jquery.ribbon.js
@@ -4,6 +4,7 @@ Copyright (c) 2009 Mikael Söderström.
 Contact: vimpyboy@msn.com
 
 Feel free to use this script as long as you don't remove this comment.
+Modified by a2h to add some extra functionality and integrate with Sponge CMS.
 */
 
 (function($) {
