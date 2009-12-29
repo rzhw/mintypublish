6aa95045fefdc319a920d1a96fcbca4249c6a7de
a2h <a2h@live.com.au>
2009-12-29 19:38:38 +1100

    Minor whitespacing change

--- trunk/spongecms/themes/default/overall.php
+++ trunk/spongecms/themes/default/overall.php
@@ -10,25 +10,26 @@
 		
 		<div id="global_wrapper">
 			<header id="header">
-			<h1 id="title"><?php echo $this->sitename; ?></h1>
-			<div id="menu_wrapper">
-				<nav>
-					<ul><?php
-					foreach ($this->getMenu() as $item)
-					{
-						if (!$item['hide'])
-						echo '
-						<li'.($item['selected']?' class="sel"':'').'>
-							<a href="'.$item['url'].'">'.$item['name'].'</a>
-						</li>';
-					}
-					echo "\n";
-					?>
-					</ul>
-				</nav>
-				<?php $this->outputAdminMenu(); ?>
-				<div style="clear:both;"></div>
-			</div>
+				<h1 id="title"><?php echo $this->sitename; ?></h1>
+				
+				<div id="menu_wrapper">
+					<nav>
+						<ul><?php
+						foreach ($this->getMenu() as $item)
+						{
+							if (!$item['hide'])
+							echo '
+							<li'.($item['selected']?' class="sel"':'').'>
+								<a href="'.$item['url'].'">'.$item['name'].'</a>
+							</li>';
+						}
+						echo "\n";
+						?>
+						</ul>
+					</nav>
+					<?php $this->outputAdminMenu(); ?>
+					<div style="clear:both;"></div>
+				</div>
 			</header>
 			
 			<section id="content">
