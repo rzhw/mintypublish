01f66a79e0390744dc25b8137bb0ad708f638fc6
a2h <a2h@live.com.au>
2010-01-08 17:30:59 +1100

    Added the copyright header to admin/files.php

--- trunk/spongecms/admin/files.php
+++ trunk/spongecms/admin/files.php
@@ -1,4 +1,27 @@
 <?php
+/*
+ * mintypublish Content Management System
+ * Copyright (c) 2009-2010 a2h
+ * http://github.com/a2h/mintypublish
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Under section 7b of the GNU General Public License you are
+ * required to preserve this notice.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 session_start();
 require_once('../config.php');
 require_once('../functions.php');
