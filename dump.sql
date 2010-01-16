0a9c9e2752700f5ffad3cdfec94f7a2ef385a8c0
a2h <a2h@live.com.au>
2010-01-16 19:59:25 +1100

    Updated SQL dump to change default page content and add "page_haschild" column

--- dump.sql
+++ dump.sql
@@ -1,16 +1,16 @@
 -- phpMyAdmin SQL Dump
--- version 3.1.3.1
+-- version 3.2.4
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 29, 2009 at 01:27 PM
--- Server version: 5.1.33
--- PHP Version: 5.2.9
+-- Generation Time: Jan 16, 2010 at 07:57 PM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
--- Database: `spongecms`
+-- Database: `mintypublish`
 --
 
 -- --------------------------------------------------------
@@ -19,7 +19,6 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `config`
 --
 
-DROP TABLE IF EXISTS `config`;
 CREATE TABLE IF NOT EXISTS `config` (
   `config_name` varchar(32) NOT NULL,
   `config_value` text
@@ -31,7 +30,8 @@ CREATE TABLE IF NOT EXISTS `config` (
 
 INSERT INTO `config` (`config_name`, `config_value`) VALUES
 ('timezone', 'Australia/Sydney'),
-('language', 'en');
+('language', 'en'),
+('sitename', 'My Unnamed Website');
 
 -- --------------------------------------------------------
 
@@ -39,12 +39,11 @@ INSERT INTO `config` (`config_name`, `config_value`) VALUES
 -- Table structure for table `media`
 --
 
-DROP TABLE IF EXISTS `media`;
 CREATE TABLE IF NOT EXISTS `media` (
   `media_id` int(11) NOT NULL AUTO_INCREMENT,
   `media_filename` varchar(64) NOT NULL,
   PRIMARY KEY (`media_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `media`
@@ -60,7 +59,6 @@ INSERT INTO `media` (`media_id`, `media_filename`) VALUES
 -- Table structure for table `pages`
 --
 
-DROP TABLE IF EXISTS `pages`;
 CREATE TABLE IF NOT EXISTS `pages` (
   `page_id` int(11) NOT NULL AUTO_INCREMENT,
   `page_orderid` int(11) NOT NULL,
@@ -68,19 +66,20 @@ CREATE TABLE IF NOT EXISTS `pages` (
   `page_title_full` varchar(64) NOT NULL,
   `page_content` longtext NOT NULL,
   `page_hideinmenu` tinyint(1) NOT NULL DEFAULT '0',
+  `page_haschild` tinyint(1) NOT NULL,
   `page_childof` int(11) NOT NULL DEFAULT '-1',
   `page_dateadded` datetime NOT NULL,
   `page_dateedited` datetime NOT NULL,
   `page_editcount` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`page_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pages`
 --
 
-INSERT INTO `pages` (`page_id`, `page_orderid`, `page_title_menu`, `page_title_full`, `page_content`, `page_hideinmenu`, `page_childof`, `page_dateadded`, `page_dateedited`, `page_editcount`) VALUES
-(1, 0, 'home', 'Home', '<p>Hello and welcome to Sponge CMS! It''s in the late alpha stages currently.</p>\r\n<p>It is not recommended you use this script in a production environment in its current state.</p>\r\n<p>The username/password pair is currently admin/admin. Change the password if you like but currently you will need to manually write something to output a password.</p>', 0, -1, '2009-06-23 09:20:49', '2009-06-23 09:20:49', 0);
+INSERT INTO `pages` (`page_id`, `page_orderid`, `page_title_menu`, `page_title_full`, `page_content`, `page_hideinmenu`, `page_haschild`, `page_childof`, `page_dateadded`, `page_dateedited`, `page_editcount`) VALUES
+(1, 0, 'home', 'Home', '<p>Hello and welcome to your mintypublish installation!</p>\n<p>Please note that it is not recommended you use this script in a production environment in its current state.</p>\n<p>The username/password pair is currently admin/admin. Change the password if you like but currently you will need to manually write something to output a password.</p>', 0, 0, -1, '2009-06-23 09:20:49', '2009-12-03 20:47:10', 0);
 
 -- --------------------------------------------------------
 
@@ -88,7 +87,6 @@ INSERT INTO `pages` (`page_id`, `page_orderid`, `page_title_menu`, `page_title_f
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_username` varchar(48) NOT NULL,
@@ -97,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_password_salt` varchar(256) NOT NULL,
   `user_group` int(11) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
