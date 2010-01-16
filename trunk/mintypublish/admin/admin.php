262be584e144cee93958fec9d87cf589baa4f578
a2h <a2h@live.com.au>
2010-01-16 15:06:26 +1100

    Page editing in the old admin panel removed (FINALLY WHOO *dance dance dance*)

--- trunk/mintypublish/admin/admin.php
+++ trunk/mintypublish/admin/admin.php
@@ -41,9 +41,6 @@ if ($zvfpcms)
 		echo '
 		<nav id="admin_menu">
 			<ul>
-				<li onclick="location.href=\''.$location['admin'].'&amp;s=man\'">
-					<img src="'.$location['images'].'/admin_man_pg.png" alt="" /> Pages
-				</li>
 				<li onclick="location.href=\''.$location['admin'].'&amp;s=cfg\'">
 					<img src="'.$location['images'].'/admin_man_cfg.png" alt="" /> Config
 				</li>
@@ -54,29 +51,6 @@ if ($zvfpcms)
 		// What subaction?
 		switch ($_GET["s"])
 		{
-			case "add":
-				include($location['admin2'].'/page_man_add.php');
-			case "man":
-				echo '<h2>'.$txt['admin_panel_manpages'].'</h2>
-				<br />';
-				
-				if (!isset($_GET["action"]))
-				{
-					include($location['admin2'].'/page_man_list.php');
-				}
-				else
-				{
-					switch ($_GET["action"])
-					{
-						case "edt":
-							include($location['admin2'].'/page_man_edit.php');
-							break;
-						default:
-							echo '<h3>'.$txt['admin_panel_what'].'</h3>'.$txt['admin_panel_actn_noexist'];
-							break;
-					}
-				}
-				break;
 			case "med":
 				echo '<h2>Manage Media</h2>
 				<br />';
