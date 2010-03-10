87250889a7781758b498ffddddf65d390405aca0
a2h <a2h@live.com.au>
2010-03-10 20:52:42 +1100

    Added new classes for floating and changed admin bar to be a bit more semantic

--- trunk/mintypublish/themes/default/admin_menu.php
+++ trunk/mintypublish/themes/default/admin_menu.php
@@ -1,11 +1,13 @@
 <aside id="admin">
+	<div class="blocks left">
 	<div class="block mode" data-type="preview">
 		<img src="<?php echo $location['images']; ?>/admin_preview.png" alt="P" title="Preview Mode" />
 	</div>
 	<div class="block mode off" data-type="edit">
 		<img src="<?php echo $location['images']; ?>/admin_edit.png" alt="E" title="Edit Mode" />
 	</div>
-	<div style="float:right;">
+	</div>
+	<div class="blocks right">
 		<div class="block button" data-type="files">
 			<img src="<?php echo $location['images']; ?>/admin_files.png" alt="Files" title="Files" />
 		</div>
